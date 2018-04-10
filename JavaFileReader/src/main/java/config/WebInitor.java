e45256615bdf1ce4cd47885a6aebd987868a9f6d
m <m@m-PC>
2018-04-10 14:59:31 +0800

    create base project 1.the code recommendation is not work normal!

---
+++ JavaFileReader/src/main/java/config/WebInitor.java
@@ -0,0 +1,25 @@
+package config;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class WebInitor extends AbstractDispatcherServletInitializer{
+
+	@Override
+	protected WebApplicationContext createServletApplicationContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected WebApplicationContext createRootApplicationContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
